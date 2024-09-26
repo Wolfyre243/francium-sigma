62a99263e818d536472364e33d96009e4f087018
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-09-26 16:01:48 +0800

    added database access

--- routes/francium.js
+++ routes/francium.js
@@ -1,29 +1,47 @@
 // This file contains all the routes for "test".
 
+//--------------------------------------------------------MARK: Import necessary tools----------------------------------------------------------------
 // Import langchain tools
 import { Ollama, OllamaEmbeddings } from '@langchain/ollama';
 import { PromptTemplate } from '@langchain/core/prompts';
 import { HttpResponseOutputParser } from 'langchain/output_parsers';
 
+// Import pgvector configurations and langchain tools
+import { PGVectorStore } from '@langchain/community/vectorstores/pgvector'
+import { pgDocumentsConfig } from '../databasing/database.js';
+
+// Import express stuff and create the router
 import express from 'express';
 const router = express.Router();
 
-// Create a simple prompt template
-const basePrompt = PromptTemplate.fromTemplate("Chat History: {chat_history}\nuser: {message}");
-
+// MARK: Set up Ollama related stuff
 const ollama = new Ollama({
     baseUrl: 'http://host.docker.internal:11434',
     model: 'aegis:v0.3',
     keepAlive: '-1'
 });
 
+// Use an embedding LLM to create embeds
+const embeddings = new OllamaEmbeddings({
+    baseUrl: "http://host.docker.internal:11434"
+})
+
+// Create a simple prompt template
+const basePrompt = PromptTemplate.fromTemplate("Chat History: {chat_history}\nuser: {message}");
+
+// MARK: Set up PG Vector Database
+const pgvectorStore = await PGVectorStore.initialize(embeddings, pgDocumentsConfig);
+
+// Define helper functions
+
 // A helper function to format messages
 const formatMessage = (role, messageContent) => {
     return `${role}: ${messageContent}`;
 };
 
-let prev_messages = '';
+let prev_messages = ''; // This stores the previous messages as a string.
 
+//------------------------------------------------- MARK: Start defining routes------------------------------------------------------------------------
 router.post('/', async (req, res) => {
     try {
         const userMessage = req.body.message;
@@ -50,8 +68,22 @@ router.post('/', async (req, res) => {
 
 // TODO: consider implementing streaming here next time
 
-router.get('/extra', (req, res) => {
-    res.send("Hello world! You found me!")
+router.get('/pgvector', async (req, res) => {
+    try {
+        const filter = { source: 'https://example.com' };
+
+        const similaritySearchResults = await pgvectorStore.similaritySearch(
+            "cat", // this is the mandatory query string
+            2, // this is the number of results to return; it is optional.
+            filter, // this is the optional filter to use.
+            // you may wish to use callbacks here as well, to handle success.
+        );
+
+        res.json(similaritySearchResults);
+
+    } catch (e) {
+        console.log(e);
+    };
 })
 
 export default router; // This will make the router available in the main app.js file.
--- testapi.js
+++ testapi.js
@@ -1,20 +1,29 @@
 import readline from 'readline-sync';
 
-while (true) {
+// while (true) {
 
-    const input = readline.question("You\n>> ");
+//     const input = readline.question("You\n>> ");
 
-    const data = await fetch("http://host.docker.internal:3030/francium", {
-        method: "POST",
-        body: JSON.stringify({
-            message: input
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+//     const data = await fetch("http://host.docker.internal:3030/francium", {
+//         method: "POST",
+//         body: JSON.stringify({
+//             message: input
+//         }),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
     
-    const dataJSON = await data.json();
+//     const dataJSON = await data.json();
     
-    console.log(dataJSON.result);
-}
+//     console.log(dataJSON.result);
+// }
+
+const data = await fetch("http://localhost:3030/francium/pgvector", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
+
+console.log(data);
