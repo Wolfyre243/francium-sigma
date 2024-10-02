16f9bf45ad7cdcfdcb685f94d24faa8439c60d97
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-10-02 12:23:23 +0800

    cleaned up api tester

--- testapi.js
+++ testapi.js
@@ -23,34 +23,3 @@ while (true) {
     
     console.log("\n", dataJSON.result, "\n");
 }
-
-// const data = await fetch("http://localhost:3030/texttospeech", {
-//     method: "POST",
-//     body: JSON.stringify({
-//         message: "Hello! I am Alyssa, and this is a test output file.",
-//     }),
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// });
-
-// const dataJSON = await data.json();
-
-// console.log(dataJSON);
-
-// const output = await fetch(`http://localhost:3030/texttospeech`, {
-//     method: "POST",
-//     body: JSON.stringify({
-//         message: "Hello! I am Alyssa, and this is a test output file.",
-//     }),
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// })
-
-// const outputJSON = await output.json();
-// console.log(outputJSON);
-
-// const wav = new wavefile.WaveFile();
-// wav.fromScratch(1, outputJSON.sampling_rate, '32f', outputJSON.audio);
-// fs.writeFileSync('output.wav', wav.toBuffer());
